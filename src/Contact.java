7245204aaf386b5b08363d76049fd6e71c04f3df
Kirill <dovganhyuk13@mail.ru>
2023-09-24 21:20:37 +0300

    Update Contact.java

--- src/Contact.java
+++ src/Contact.java
@@ -2,24 +2,22 @@ import java.util.Objects;
 
 public record Contact(String name,
                       String email) {
-    public Contact{
+    public Contact {
         Objects.requireNonNull(name);
         Objects.requireNonNull(email);
         if (name.isEmpty()) {
             throw new IllegalArgumentException("Name can't be empty");
         }
-        if (!email.isEmpty()){
-            if (!email.endsWith("@gmail.com")){
+        if (!email.isEmpty()) {
+            if (!email.endsWith("@gmail.com")) {
                 throw new IllegalArgumentException("It is not an email");
             }
-        }
-        else {
+        } else {
             email = "testEmail@gmail.com";
         }
     }
 
-    public void sayHello(){
+    public void sayHello() {
         System.out.println("Hello, " + name);
     }
-
 }
