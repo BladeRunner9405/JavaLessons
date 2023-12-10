e03e3bcd1dd5fbfabd048ca724cbf03fd1d2d4b7
Kirill <dovganhyuk13@mail.ru>
2023-12-10 19:49:37 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        Function<Integer, Integer> f2 = bind((a, b) -> a * b, 2);
-        System.out.println(f2.apply(12));
+        Function<Integer, Integer> f1 = bind((a, b) -> a * b, 2);
+        System.out.println(f1.apply(5));
+        Function<String, String> f2 = bind((a, b) -> b.substring(0, a), 5);
+        System.out.println(f2.apply("1234567899"));
     }
     static <A, B, C> Function<B, C> bind(BiFunction<A, B, C> fn, A a) {
         return b -> fn.apply(a, b);
